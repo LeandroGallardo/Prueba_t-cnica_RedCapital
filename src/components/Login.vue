e4e6b370e8b20d49d08cdd8d431d08fbf063a4f2
leandro gallardo <l.gallardo.fernandez00@gmail.com>
2020-03-03 00:26:06 -0300

    integracion bootstrap y formulario inicial

---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <form action="">
+          <div class="form-group">
+            <label for="nombre" class="h5">Nombre</label>
+            <input type="text"
+              class="form-control"
+              id="nombre"
+             />
+          </div>
+          <div class="form-group">
+            <label for="nombre" class="h5">Contraseña</label>
+            <input type="password"
+                  class="form-control" 
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
